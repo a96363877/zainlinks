53cfa2285d1dcc4eadbaa4dc0cda5d63af32fb07
a96363877 <a96363877@gmail.com>
2025-06-04 12:09:42 +0300

    dsffd

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Share2, QrCode, Info, ChevronRight, Wifi, Battery, Signal } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useRouter } from "next/navigation"
