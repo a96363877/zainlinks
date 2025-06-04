83cb5a41b6b7619ef1579835a24f2150796b1af5
a96363877 <a96363877@gmail.com>
2025-06-04 12:11:43 +0300

    ss

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Zain App',
+  title: 'Links App',
   description: 'The best app for links',
 };
 
